0be13d2e5c3a68aae87d3f3d6bf88afecda7010f
Kirill Shestakov <fakie215@gmail.com>
2017-11-13 00:52:48 +0200

    Initial project structure. Entry point script base

---
+++ didww-aptly-cli-lib/defaults.py
@@ -0,0 +1,11 @@
+defaults = {
+        "global": {
+            "url": "http://10.15.70.6:8090/api",
+            },
+        "publish": {
+            "passphraze_file": "/home/pkg/gpg_pass",
+            },
+        "files": {
+            "upload_timeout": 300,
+            },
+        }
