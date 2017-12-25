a41a1250033baf14a56b61f8c1450ccff22b0670
Kirill Shestakov <fakie215@gmail.com>
2017-12-25 22:55:44 +0200

    rework copy plugin

--- didww_aptly_ctl/plugins/copy.py
+++ didww_aptly_ctl/plugins/copy.py
@@ -7,95 +7,157 @@ from re import match
 from aptly_api import Client
 from aptly_api.base import AptlyAPIException
 from didww_aptly_ctl.exceptions import DidwwAptlyCtlError
-from didww_aptly_ctl.utils.misc import publish_update
+from didww_aptly_ctl.utils.misc import lookup_publish_by_repos, flatten_list
+from didww_aptly_ctl.defaults import defaults
 from didww_aptly_ctl.utils import AptlyKey
+from didww_aptly_ctl.utils import SerializedIO
 
 logger = logging.getLogger(__name__)
 
 def config_subparser(subparsers_action_object):
-    parser_copy = subparsers_action_object.add_parser("copy",
-            description="Copies/Moves packages between components of the same distribution.",
-            help=       "Copies/Moves packages between components of the same distribution.")
+    desc_msg = """
+    Copies/moves packages between local repos and updates dependent publishes.
+    Outputs a dict with target repo as key, and list of successfully copied
+    packages as value. By default, works in the mode 'copy all valid or nothing'.
+    With -k prints warnings, but performs valid actions.
+    Flag -f allows to copy between repos with different default distributions.
+    """
+    help_msg = """
+    Copies/Moves packages between local repos and updates dependent publishes
+    """
+    parser_copy = subparsers_action_object.add_parser("copy", description=desc_msg, help=help_msg)
+
     parser_copy.set_defaults(func=copy)
-    parser_copy.add_argument("-s", "--source", required=True,
-            help="Triplet pointing to source repo. E.g. --source jessie_billing_unstable")
-    parser_copy.add_argument("-t", "--target", required=True,
-            help="Triplet pointing to target repo. E.g. --target jessie_billing_stable")
-    parser_copy.add_argument("refs", metavar="package_referece", nargs="+",
-            help="Can be direct referece (e.g. 'aptly_0.9~dev+217+ge5d646c_i386') or key \
-                    (e.g. 'Pi386 libboost-program-options-dev 1.49.0.1 918d2f433384e378').")
-    parser_copy.add_argument("-m", "--move", action="store_true",
-            help="Remove packages from source.")
-    parser_copy.add_argument("-k", "--keep-going", action="store_true",
-            help="Keep going even if failed to find some packages in source repo.")
+
+    help_msg = """
+    Target repo name. E.g. --target jessie_billing_stable
+    """
+    parser_copy.add_argument("-t", "--target", required=True, help=help_msg)
+
+    help_msg = """
+    source_repo is a repo name from where to copy package, and package_reference is an
+    aplty key of a package (e.g. 'Pi386 libboost-program-options-dev 1.49.0.1 918d2f433384e378').
+    If '-' is supplied, reads a source refs list from stdin in a form of
+    a dictionary, where keys are repo names, and values are lists of aptly
+    keys (e.g. read output of search plugin)
+    """
+    parser_copy.add_argument("refs", metavar="source_repo/package_referece", nargs="+", help=help_msg)
+
+    help_msg = "Remove packages from source"
+    parser_copy.add_argument("-m", "--move", action="store_true", help=help_msg)
+
+    help_msg = "Keep going even if errors occur"
+    parser_copy.add_argument("-k", "--keep-going", action="store_true", help=help_msg)
+
+    help_msg = "Allow copy when the default distribution of a source and a target do not match"
+    parser_copy.add_argument("-f", "--force", action="store_true", help=help_msg)
+
+    help_msg = "Don not copy, delete or update publish. Just validate actions and show is to be done"
+    parser_copy.add_argument("--dry-run", action="store_true", help=help_msg)
+
+
+def get_repo_info(client, repo):
+    try:
+        return client.repos.show(repo)
+    except AptlyAPIException as e:
+        if e.status_code == 404:
+            raise DidwwAptlyCtlError("Repo '{}' doest not exist".format(args.target))
+        else:
+            raise
 
 
 def copy(args):
-    s_release = args.source.split("_")[0]
-    t_release = args.target.split("_")[0]
-    if s_release != t_release:
-        raise DidwwAptlyCtlError("You can't move from {} release to {}".format(s_release, t_release))
     aptly = Client(args.url)
+    io = SerializedIO(input_f='stdin', output_f='stdout', output_f_fmt=args.fmt)
 
-    #search package refs in source repo and get their keys (or validate if key supplied)
-    dir_refs_to_validate = []
-    for r in args.refs:
-        if AptlyKey.key_regexp.match(r):
-            dir_ref = AptlyKey(r).getDirRef()
-            logger.debug("Converting '{}' to '{}'".format(r, dir_ref))
-            dir_refs_to_validate.append(dir_ref)
-        elif AptlyKey.dir_ref_regexp.match(r):
-            dir_refs_to_validate.append(r)
-        else:
-            raise DidwwAptlyCtlError("Incorrect package reference: %s" % r)
+    # construct source refs dict
+    if args.refs[0] == "-":
+        logger.info("Getting source refs from stdin")
+        source_refs = io.get_input()
+    else:
+        source_refs = dict()
+        for r in args.refs:
+            repo, _, ref = r.partition("/")
+            if ref == "":
+                raise DidwwAptlyCtlError("Incorrect source reference name: '{}'".format(r))
+            elif repo in source_refs:
+                source_refs[repo].append(ref)
+            else:
+                source_refs[repo] = [ ref ]
 
-    keys = []
-    for r in dir_refs_to_validate:
+    if source_refs is None or len(source_refs) == 0:
+        raise DidwwAptlyCtlError("Source list is empty")
+
+    # check for invalid actions
+    errors = False
+    target_repo = get_repo_info(aptly, args.target)
+    logger.debug("Source refs before validation: {}".format(source_refs))
+    for repo, refs in source_refs.copy().items():
         try:
-            search_result = aptly.repos.search_packages(args.source, r)
-        except AptlyAPIException as e:
-            if e.status_code == 404:
-                raise DidwwAptlyCtlError("Failed to search for package by direct referece.", e)
-            else:
-                raise
-        else:
-            if len(search_result) == 0:
-                if args.keep_going:
-                    logger.warn("Failed to find {} in repo {}".format(r, args.source))
-                else:
-                    raise DidwwAptlyCtlError("Failed to find {} package in repo {}".format(r, args.source))
-            elif len(search_result) == 1:
-                keys.append(search_result[0][0])
-            else:
-                logger.warn("Skipping {}. Search by direct reference returned many results: {}".format(dir_ref, keys))
-                if not args.keep_going:
-                    raise DidwwAptlyCtlError("Failed to find {} package in repo {}".format(r, args.source))
+            repo_info = get_repo_info(aptly, repo)
+            if not args.force and repo_info.default_distribution != target_repo.default_distribution:
+                raise DidwwAptlyCtlError("Default distributions of the source repo " \
+                        + "'{}', '{}'".format(repo_info.name, repo_info.default_distribution) \
+                        + " does not match one of the target repo " \
+                        + "'{}', '{}'.".format(target_repo.name, target_repo.default_distribution) \
+                        + " Supply --force to ignore this")
+        except DidwwAptlyCtlError as e:
+            logger.warn(e)
+            erros = True
+            del source_refs[repo]
+            break
+        for ref in refs[:]:
+            key = AptlyKey(ref, repo)
+            if not key.exists(aptly):
+                logger.warn("There is no '{}' in '{}'".format(ref, repo))
+                erros = True
+                source_refs[repo].remove(ref)
+    logger.debug("Source refs after validation: {}".format(source_refs))
 
-    if len(keys) == 0: 
-        raise DidwwAptlyCtlError("Could not find any package ref in source repo.")
+    if errors and not args.keep_going:
+        raise DidwwAptlyCtlError("There are invalid actions. Invoke with --keep-going to do valid actions.")
+    elif errors and args.keep_going:
+        logger.warn("Skipping invalid actions...")
+    if len(source_refs) == 0: 
+        raise DidwwAptlyCtlError("There are no valid actions.")
 
     # Copy packages
-    logger.info("Copying packages below from {} to {}".format(args.source, args.target))
-    for k in keys:
-        logger.info("    " + k)
+    keys = flatten_list(source_refs.values())
+    keys.sort(key=lambda key: AptlyKey(key))
+    logger.debug("keys to copy: {}".format(keys))
 
     #TODO handle case when some of pakcages connot be copied because of conflict
     try:
-        add_result = aptly.repos.add_packages_by_key(args.target, *keys)
+        if not args.dry_run:
+            add_result = aptly.repos.add_packages_by_key(args.target, *keys)
+            logger.debug("add result: {}".format(add_result))
     except AptlyAPIException as e:
-        if e.status_code in [404, 400]:
-            raise DidwwAptlyCtlError("Failed to copy packages.", e)
+        if e.status_code == 400:
+            raise DidwwAptlyCtlError("Failed to copy packages", e)
         else:
             raise
 
     # Delete source if --move
     if args.move:
-        delete_result = aptly.repos.delete_packages_by_key(args.source, *keys)
+        for repo, refs in source_refs.items():
+            logger.debug("deleting {} from {}".format(refs, repo))
+            if not args.dry_run:
+                delete_result = aptly.repos.delete_packages_by_key(repo, *refs)
+                logger.debug("delete result: {}".format(delete_result))
 
     # Update publishes
-    update_result = publish_update(aptly, s_release, s_release, args.pass_file)
-    logger.info("Updated publish {0}/{0}".format(s_release))
-    logger.debug(update_result)
+    pubs = lookup_publish_by_repos(aptly, list(source_refs.keys()) + [target_repo.name])
+    for p in pubs:
+        if not args.dry_run:
+            update_result = aptly.publish.update(
+                    prefix = p.prefix,
+                    distribution = p.distribution,
+                    sign_gpgkey = defaults["publish"]["gpg_key_name"],
+                    sign_passphrase_file = defaults["publish"]["passphraze_file"]
+                    )
+            logger.debug(update_result)
+        logger.info("Updated publish {}/{}".format(p.prefix, p.distribution))
 
+    io.print_output({args.target: keys})
     return 0
 
