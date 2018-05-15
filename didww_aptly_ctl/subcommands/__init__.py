ef1dbfdf1688db7812ec8a72e06a111d9e38cefb
Kirill Shestakov <fakie215@gmail.com>
2018-05-15 23:34:31 +0300

    remove old versions of copy, remove, search subcommands

--- didww_aptly_ctl/subcommands/__init__.py
+++ didww_aptly_ctl/subcommands/__init__.py
@@ -1,13 +1,7 @@
 from didww_aptly_ctl.subcommands import (
         put,
-        search,
-        copy,
-        remove
         )
 
 __all__ = [
         "put",
-        "search",
-        "copy",
-        "remove"
         ]
