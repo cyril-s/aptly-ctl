8c3a604a6913b66e06062a18f376897dfd970267
Kirill Shestakov <kirill.sh@didww.com>
2018-05-16 13:12:10 +0300

    add basic search subcommand

--- didww_aptly_ctl/subcommands/__init__.py
+++ didww_aptly_ctl/subcommands/__init__.py
@@ -1,9 +1,11 @@
 from didww_aptly_ctl.subcommands import (
         put,
         remove,
+        search,
         )
 
 __all__ = [
         "put",
         "remove",
+        "search",
         ]
---
+++ didww_aptly_ctl/subcommands/search.py
@@ -0,0 +1,58 @@
+import logging
+import json
+from didww_aptly_ctl.utils.ExtendedAptlyClient import ExtendedAptlyClient
+from aptly_api.base import AptlyAPIException
+from didww_aptly_ctl.exceptions import DidwwAptlyCtlError
+from didww_aptly_ctl.utils.PackageRef import PackageRef
+
+logger = logging.getLogger(__name__)
+
+def config_subparser(subparsers_action_object):
+    parser_search = subparsers_action_object.add_parser("search",
+            help="Search packages in local repos.",
+            description="Search packages in local repos.")
+    parser_search.set_defaults(func=search)
+
+    parser_search.add_argument("queries", metavar="query", nargs="+",
+            help="Query in format documented at https://www.aptly.info/doc/feature/query/.")
+
+    parser_search.add_argument("-r", "--repo", dest="repos", action="append",
+            help="Limit search to specified repos.")
+
+    #parser_search.add_argument("-n", "--name", action="store_true",
+    #        help="Treat query as regex of package's name.")
+
+    #parser_search.add_argument("--pretty", action="store_true",
+    #        help="Print more readable rather than parsable output.")
+
+
+def search(config, args):
+    aptly = ExtendedAptlyClient(config["url"])
+
+    if args.repos:
+        repo_list = args.repos[:]
+    else:
+        search_result = aptly.repos.list()
+        repo_list = [ r[0] for r in search_result ]
+        if len(repo_list) == 0:
+            raise DidwwAptlyCtlError("Seems aptly doesn't have any local repos.")
+    logger.info("Searching in repos {}".format(", ".join(repo_list)))
+    repo_list.sort()
+
+    try:
+        for q in args.queries:
+            logger.debug("Query: " + q)
+            for r in repo_list:
+                search_result = aptly.repos.search_packages(r, q)
+                searched_list = [ s.key for s in search_result ]
+                logger.debug("For query '{}' in repo '{}' api returned: {}".format(q, r, search_result))
+                searched_list.sort(key=lambda key: PackageRef(key))
+                for s in searched_list:
+                    print('"{}/{}"'.format(r, s))
+    except AptlyAPIException as e:
+        if e.status_code in [404, 400]:
+            raise DidwwAptlyCtlError("Failed to search packages.", e)
+        else:
+            raise
+
+    return 0
