763572ad00f11afd49be6ffa6fe0189f3e8c23d7
Kirill Shestakov <fakie215@gmail.com>
2018-08-16 18:13:51 +0300

    add create, delete and edit for repo subcommand

--- didww_aptly_ctl/subcommands/repo.py
+++ didww_aptly_ctl/subcommands/repo.py
@@ -9,28 +9,34 @@ logger = logging.getLogger(__name__)
 
 def config_subparser(subparsers_action_object):
     parser_repo = subparsers_action_object.add_parser("repo",
-            help="List, show, create, edit and delete local repos.",
-            description="List, show, create, edit and delete local repos.")
-    parser_repo.set_defaults(func=repo)
+            help="Administer local repos",
+            description="Administer local repos")
+    subparsers = parser_repo.add_subparsers()
 
-    action_group = parser_repo.add_mutually_exclusive_group(required=True)
-    action_group.add_argument("-l", "--list", action="store_true",
-            help="List local repos.")
+    parser_list = subparsers.add_parser("list", help="List local repos")
+    parser_list.set_defaults(func=list)
+    parser_list.add_argument("--detail", action="store_true",
+            help="Print additional details when showing and listing")
 
-    action_group.add_argument("-s", "--show", metavar="repo",
-            help="Show info about a local repo.")
+    parser_create = subparsers.add_parser("create", help="Create a local rep")
+    parser_create.set_defaults(func=create)
+    parser_create.add_argument("name", help="Name of new repo")
+    parser_create.add_argument("--comment", help="Text describing local repository for a user")
+    parser_create.add_argument("--dist", help="Default distribution when publishing from this local repo")
+    parser_create.add_argument("--comp", help="Default component when publishing from this local repo")
 
-    action_group.add_argument("-c", "--create", metavar="repo",
-            help="Create a local repo.")
+    parser_edit = subparsers.add_parser("edit", help="Edit a local repo")
+    parser_edit.set_defaults(func=edit)
+    parser_edit.add_argument("name", help="Name of a repo")
+    parser_edit.add_argument("--comment", help="Text describing local repository for a user")
+    parser_edit.add_argument("--dist", help="Default distribution when publishing from this local repo")
+    parser_edit.add_argument("--comp", help="Default component when publishing from this local repo")
 
-    action_group.add_argument("-e", "--edit", metavar="repo",
-            help="Edit a local repo.")
-
-    action_group.add_argument("-d", "--delete", metavar="repo",
-            help="Delete a local repo.")
-
-    parser_repo.add_argument("--detail", action="store_true",
-            help="Print additional details when showing and listing.")
+    parser_delete = subparsers.add_parser("delete", help="Delete a local repo")
+    parser_delete.set_defaults(func=delete)
+    parser_delete.add_argument("name", help="Name of a repo")
+    parser_delete.add_argument("-f", "--force", action="store_true",
+            help="Delete local repository even if it has snapshots")
 
 
 def pprint_repo(repo, packages=[]):
@@ -45,31 +51,55 @@ def pprint_repo(repo, packages=[]):
             print(" "*8 + '"%s"' % p.key)
 
 
-def repo(config, args):
+def list(config, args):
     aptly = ExtendedAptlyClient(config["url"])
-
-    if args.list:
-        repo_list = aptly.repos.list()
-        repo_list.sort(key=lambda k: k.name)
-        for r in repo_list:
-            if args.detail:
-                pprint_repo(r)
-            else:
-                print(r.name)
-    elif args.show:
-        try:
-            show_result = aptly.repos.show(args.show)
-        except AptlyAPIException as e:
-            if e.status_code == 404 and "local repo with name" in e.args[0].lower():
-                raise DidwwAptlyCtlError(e)
-            else:
-                raise
+    repo_list = aptly.repos.list()
+    repo_list.sort(key=lambda k: k.name)
+    for r in repo_list:
         if args.detail:
-            search_result = aptly.repos.search_packages(args.show)
-            pprint_repo(show_result, search_result)
+            pprint_repo(r)
         else:
-            pprint_repo(show_result)
-    else:
-        raise DidwwAptlyCtlError(NotImplementedError("Command not yet implemented"))
-
+            print(r.name)
     return 0
+
+
+def create(config, args):
+    aptly = ExtendedAptlyClient(config["url"])
+    try:
+        create_result = aptly.repos.create(args.name, args.comment, args.dist, args.comp)
+    except AptlyAPIException as e:
+        if e.status_code == 400:
+            raise DidwwAptlyCtlError(e)
+        else:
+            raise
+    else:
+        logger.info("Created repo {}".format(create_result))
+        return 0
+
+
+def edit(config, args):
+    aptly = ExtendedAptlyClient(config["url"])
+    try:
+        edit_result = aptly.repos.edit(args.name, args.comment, args.dist, args.comp)
+    except AptlyAPIException as e:
+        if e.status_code in [0, 404]:
+            raise DidwwAptlyCtlError(e)
+        else:
+            raise
+    else:
+        logger.info("Edited repo: {}".format(edit_result))
+        return 0
+
+
+def delete(config, args):
+    aptly = ExtendedAptlyClient(config["url"])
+    try:
+        aptly.repos.delete(args.name, args.force)
+    except AptlyAPIException as e:
+        if e.status_code in [404, 409]:
+            raise DidwwAptlyCtlError(e)
+        else:
+            raise
+    else:
+        logger.info("Deleted repo %s" % args.name)
+        return 0
