88dfae0aa1b46615ba4b129765f15df4f11e3733
Kirill Shestakov <fakie215@gmail.com>
2018-09-07 14:24:56 +0300

    add PubSpec class for identifying publishes

---
+++ didww_aptly_ctl/utils/PubSpec.py
@@ -0,0 +1,26 @@
+class PubSpec:
+
+    def __init__(self, distribution, prefix="."):
+        if '/' in distribution:
+            pref, sep, dist = distribution.partition('/')
+            if len(pref) == 0 or len(dist) == 0:
+                raise ValueError("refspec invalid")
+            self._prefix = pref
+            self._distribution = dist
+        else:
+            self._prefix = prefix
+            self._distribution = distribution
+
+    @property
+    def prefix(self):
+        return self._prefix
+
+    @property
+    def distribution(self):
+        return self._distribution
+
+    def __repr__(self):
+        return self.prefix + "/" + self.distribution
+
+    def __str__(self):
+        return self.__repr__()
