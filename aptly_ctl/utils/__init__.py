ce6c1c3ed30cd2c8cd6db08367c8bc034f38a4ca
Kirill Shestakov <fakie215@gmail.com>
2018-11-15 18:08:19 +0200

    add basic PackageFile class that represents deb file

--- aptly_ctl/utils/__init__.py
+++ aptly_ctl/utils/__init__.py
@@ -1,4 +1,5 @@
 from aptly_ctl.utils.Version import Version
 from aptly_ctl.utils.PackageRef import PackageRef
+from aptly_ctl.utils.PackageFile import PackageFile
 from aptly_ctl.utils.PubSpec import PubSpec
 from aptly_ctl.utils import misc
