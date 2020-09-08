ecc9c444ab8556477ac70fd1f70a954e91f9ad1e
Kirill Shestakov <freyr.sh@gmail.com>
2020-09-08 16:51:45 +0300

    remove obsolete code

--- aptly_ctl/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from aptly_ctl.utils.Version import Version
-from aptly_ctl.utils.PubSpec import PubSpec
-from aptly_ctl.utils.PackageFile import PackageFile
-from aptly_ctl.utils.PackageRef import PackageRef
-from aptly_ctl.utils.ExtendedAptlyClient import ExtendedAptlyClient
-from aptly_ctl.utils import misc
